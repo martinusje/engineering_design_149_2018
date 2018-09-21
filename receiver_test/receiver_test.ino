d46efcbce4a2566d6259d2d76349bec174e1b0f4
Martijn Verhoeven <martijn264@gmail.com>
2018-09-21 12:39:46 +0200

    Update receiver test to use RadioHead library

--- receiver_test/receiver_test.ino
+++ receiver_test/receiver_test.ino
@@ -1,13 +1,30 @@
-#define RECEIVE_PIN 5
+#include <RH_ASK.h>
+#include <SPI.h>
+
+#define TRANSMIT_PIN 8 //Incorrect! but needed because the motor is also on pin 5
+#define RECEIVE_PIN 2
+#define SHUT_PIN 4
+
+RH_ASK driver(2000, RECEIVE_PIN, TRANSMIT_PIN);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(RECEIVE_PIN, INPUT);
-
+  pinMode(SHUT_PIN, OUTPUT);
+  digitalWrite(SHUT_PIN, LOW);
+  
   Serial.begin(9600);
+  
+  if (!driver.init())
+    Serial.println("rf driver init failed");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(digitalRead(RECEIVE_PIN));
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN] = {0};
+  uint8_t buflen = sizeof(buf);
+ 
+  if (driver.recv(buf, &buflen)) // if message received, save it
+  {
+    Serial.println((char*)buf); // print received message
+  }
 }
