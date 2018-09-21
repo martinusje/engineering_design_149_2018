c8f2b72b9716961282eb77cf5c2bda9d0be2c4d7
Martijn Verhoeven <martijn264@gmail.com>
2018-09-21 12:29:25 +0200

    Update transmitter test to use RadioHead library

--- transmitter_test/transmitter_test.ino
+++ transmitter_test/transmitter_test.ino
@@ -1,16 +1,23 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+
 #define TRANSMIT_PIN 5
 
+RH_ASK driver(2000, 2, TRANSMIT_PIN); // 200bps, TX on D3 (pin 2), RX on D4 (pin 3)
 int period = 1000;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(TRANSMIT_PIN, OUTPUT);
+  Serial.begin(9600);
+  
+  if (!driver.init())
+    Serial.println("rf driver init failed");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(TRANSMIT_PIN, HIGH);
-  delay(period/2);
-  digitalWrite(TRANSMIT_PIN, LOW);
-  delay(period/2);
+  const char *msg = "hello world";
+  driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  delay(1000);
 }
